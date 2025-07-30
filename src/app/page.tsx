d4ace514095e0cfb3390005ed77a1972c3afb0b5
Prashant Nagaraja <prashant.nagaraja@gmail.com>
2025-07-30 09:56:10 -0400

    Fetching books directly from Google and rendering with pagination

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,143 @@
-import Image from "next/image";
+"use client";
+
+import { SearchResponse } from "@/Models/Book";
+import { searchAllBooks } from "@/services/BookService";
+import { useEffect, useState } from "react";
+
+export const numberOfBooksToRender = 12;
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [query, setQuery] = useState('');
+  const [searchResults, setSearchResults] = useState<SearchResponse|null>(null);
+  const [nextPageExists, setNextPageExists] = useState<boolean>(false);
+  const [currentPage, setCurrentPage] = useState<number>(0);
+  
+  const isBrowser = () => typeof window !== 'undefined'; //The approach recommended by Next.js
 
+  function scrollToTop() {
+      if (!isBrowser()) return;
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
+
+  const fetchDataForCurrentPage = async () => {
+    console.log('cccc', currentPage);
+    const booksResponse = await searchAllBooks(query, currentPage);
+    const doesNextPageExist = booksResponse.totalItems > (currentPage * numberOfBooksToRender);
+    setNextPageExists(doesNextPageExist);
+    setSearchResults(booksResponse);
+    console.log('HEHEHEHE', booksResponse);
+  }
+  
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    fetchDataForCurrentPage();
+  };
+  
+  useEffect(
+    () => {
+      if(query !== ''){
+        fetchDataForCurrentPage();
+      }
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [currentPage]
+  )
+  return (
+    <div className="font-sans grid grid-rows-[-200px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+          <div className="flex flex-col items-center p=10"> 
+          <form className="flex flex-col gap-2 w-full max-w-xs p-10" onSubmit={handleSubmit} role='search'>
+              <input 
+                  aria-label="Search for a Book" 
+                  type="text" 
+                  placeholder="Search" 
+                  className="text-sm text-gray-base py-2 px-14 border border-gray-200 rounded"
+                  value={query} 
+                  onChange={(e) => setQuery(e.target.value)}  />
+              
+
+              <button 
+                  type="submit"
+                  className="bg-green-400 w-full mt-4">
+                  Search
+              </button>
+          </form>
+
+        
+        {searchResults && (
+        
+          <div className="grid grid-cols-2 gap-8 sm:grid-cols-3 lg:grid-cols-3">
+            {searchResults.items.map((book) => {
+              return (
+                <div key={book.id} className="pt-6">
+                  <div className="flow-root bg-light rounded-lg px-4 pb-8">
+                    <div className="-mt-6">
+                      <div className="flex items-center justify-center">
+                        <img
+                          src={
+                            // Removes compression to get higher quality
+                            book.volumeInfo.imageLinks?.smallThumbnail.replace(/._SX50_|._SY75_/gi, "")
+                          }
+                          className="p-2 w-64 rounded-lg"
+                          alt={book.volumeInfo.title}
+                        />
+                      </div>
+                      <div className="text-center justify-center items-center">
+                        <h3 className="mt-4 text-lg font-bold w-full break-words overflow-x-auto text-primary tracking-tight">
+                          {book.volumeInfo.title}
+                        </h3>
+                        <p className="mt-2 text-base leading-relaxed text-secondary">
+                          {book.volumeInfo.authors ? book.volumeInfo.authors[0] : 
+                          ''} ({book.volumeInfo.publishedDate})
+                        </p>
+                        <span className="font-bold text-secondary">
+                          Rating: {book.volumeInfo.averageRating}
+                        </span>
+                        <a
+                          href={book.volumeInfo.infoLink}
+                          target="_blank"
+                          className="mt-4 block text-active underline"
+                        >
+                          Read More
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+      )}
+
+        {nextPageExists && (
         <div className="flex gap-4 items-center flex-col sm:flex-row">
+          {currentPage > 0 && (
           <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"            
+            onClick={() => {
+              setCurrentPage(currentPage-1)
+              scrollToTop();
+            }}
             target="_blank"
             rel="noopener noreferrer"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
+            PREVIOUS
           </a>
+          )}
           <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"            
+            onClick={() => {
+              setCurrentPage(currentPage+1)
+              scrollToTop();
+            }}
             target="_blank"
             rel="noopener noreferrer"
           >
-            Read our docs
+            NEXT
           </a>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        )}
+        </div>
+      </main> 
     </div>
   );
 }
--- src/services/BookService.ts
+++ src/services/BookService.ts
@@ -1,10 +1,14 @@
+import { numberOfBooksToRender } from "@/app/page";
 import { SearchResponse } from "@/Models/Book";
 
 export async function searchAllBooks(query: string, page: number = 0): Promise<SearchResponse> {
   try {
-    const request = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${query}&key=AIzaSyCmqHggyzAjtJaO1nrtuXR-V0zLgRgLXHo`)
-    const todos = await request.json();    
-    return todos;
+    const startPoint = page * numberOfBooksToRender;
+    const url = `https://www.googleapis.com/books/v1/volumes?q=${query}&startIndex=${startPoint}&maxResults=${numberOfBooksToRender}&key=AIzaSyCmqHggyzAjtJaO1nrtuXR-V0zLgRgLXHo`;
+    console.log(url);
+    const request = await fetch(url)
+    const books = await request.json();    
+    return books;
   } catch (e) {
     console.log('Fetch Call Failed', e);
     return {} as SearchResponse;
